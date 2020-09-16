6d6cee6e3e73b5a092c09e2211430065e1a6b8ea
Christopher Dean <christopher.dean11@gmail.com>
2020-09-15 21:38:57 -0400

    init commit of cslox

---
+++ cslox/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace cslox
+{
+    class Program
+    {
+        static bool hadError = false;
+        static void Main(string[] args)
+        {
+            if (args.Length > 1){
+                Console.WriteLine("Usage: cslox [script[");
+            } else if (args.Length == 1){
+                runFile(args[0]);
+            } else if (args.Length == 0){
+                runPrompt();
+            }
+        }
+
+        private static void runFile(String path){
+            // Byte[] bytes = File.ReadAllBytes(Path.GetFullPath(path));
+            // why not just read it as a string from file??
+            // run(new String(bytes));
+
+            String txt = File.ReadAllText(Path.GetFullPath(path));
+            run(txt);
+            if (hadError) {
+                // set non-zero exit code if we had an error while running the script
+                Environment.ExitCode = 65;
+            }
+        }
+
+        private static void runPrompt(){
+            while (true){
+                Console.Write("> ");
+                String line = Console.ReadLine();
+                if (line == null){
+                    break;
+                }
+                run(line);
+                // reset hadError to keep the REPL alive
+                hadError = false;
+            }
+        }
+
+        private static void run(String source){
+            Console.WriteLine(source);
+        }
+
+        // private static void run2(String source){
+
+        //     // not yet implemented
+        //     Scanner scanner = new Scanner(source);
+        //     List<Token> tokens = scanner.scanTokens();
+
+        //     // print tokens while getting started
+        //     foreach (Token token in tokens){
+        //         Console.WriteLine(token);
+        //     }
+        // }
+
+        static void error(int line, String message){
+            report(line, "", message);
+        }
+
+        static void report(int line, String where, String message){
+            Console.WriteLine($"[Line {line}] Error {where}: {message}");
+        }
+    }   
+}
---
+++ cslox/progress.md
@@ -0,0 +1,21 @@
+# CSLOX
+### A Lox interpreter in C#
+Following this online book for jlox, but implementing in C# instead of Java
+http://www.craftinginterpreters.com/scanning.html
+
+Re-writing the Java examples in C# (dotnet core on a Mac specifically) will force me to 
+1. Actually write them myself instead of copying them if they get long or complicated
+1. Get more familiar with C#, which I've wanted for a little while
+
+## Day 1
+### Basic structure
+Got some basic structure in place with a simple echo prompt.
+```
+christopherdean@ChristohersMBP3:~/Dev/lox/cslox$ dotnet run
+> hello
+hello
+> hi
+hi
+> world
+world
+```
